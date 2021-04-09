9fef06d6ab40eba75edaa652f16487d838083d8b
dev-yakuza <dev.yakuza@gmail.com>
2021-04-09 21:56:36 +0900

    Add example

---
+++ admob_example/android/app/src/main/kotlin/com/example/admob_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.admob_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ admob_example/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+const Map<String, String> UNIT_ID = kReleaseMode
+    ? {
+        'ios': '[YOUR iOS AD UNIT ID]',
+        'android': '[YOUR ANDROID AD UNIT ID]',
+      }
+    : {
+        'ios': 'ca-app-pub-3940256099942544/2934735716',
+        'android': 'ca-app-pub-3940256099942544/6300978111',
+      };
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    TargetPlatform os = Theme.of(context).platform;
+
+    BannerAd banner = BannerAd(
+      listener: AdListener(
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {},
+        onAdLoaded: (_) {},
+      ),
+      size: AdSize.banner,
+      adUnitId: UNIT_ID[os == TargetPlatform.iOS ? 'ios' : 'android']!,
+      request: AdRequest(),
+    )..load();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Admob'),
+      ),
+      body: Center(
+        child: Container(
+          height: 50,
+          child: AdWidget(
+            ad: banner,
+          ),
+        ),
+      ),
+    );
+  }
+}
