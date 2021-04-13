ab5b19dca625672d43d1b4d6065a8829e9643efe
dev-yakuza <dev.yakuza@gmail.com>
2021-04-13 20:24:50 +0900

    add weather app example

--- examples/weather_app/lib/screens/loading.dart
+++ examples/weather_app/lib/screens/loading.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,9 +10,36 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    } catch (e) {
+      print('There was a problem with the internet connection.');
+    }
+  }
+
+  void fetchData() async {
+    http.Response response = await http.get(Uri.https(
+        'samples.openweathermap.org',
+        'data/2.5/weather',
+        {'q': 'London', 'appid': 'b1b15e88fa797225412429c1c50c122a1'}));
+    if (response.statusCode == 200) {
+      var jsonData = jsonDecode(response.body);
+      var description = jsonData['weather'][0]['description'];
+      var speed = jsonData['wind']['speed'];
+      var id = jsonData['id'];
+      print(description);
+      print(speed);
+      print(id);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    fetchData();
   }
 
   @override
@@ -18,9 +47,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            getLocation();
-          },
+          onPressed: null,
           child: Text(
             'Get my location',
             style: TextStyle(color: Colors.white),
